1c39a3e3b8e58dcc93b8de9b51c3778bd575eb07
Dan Worley <danworley@gmail.com>
2024-11-26 13:30:40 -0500

    Re-enable log

--- source/ui/data/boxedContainer.cpp
+++ source/ui/data/boxedContainer.cpp
@@ -606,8 +606,8 @@ void BoxedContainer::setWindowsVersion(const BoxedWinVersion& version) {
 
 BString BoxedContainer::getLogPath() {
     // Disabling logging in favor of console while debugging video issues as it thrashes disk
-    //this->dirPath.stringByApppendingPath("lastLog.txt");
-    return B(""); 
+    this->dirPath.stringByApppendingPath("lastLog.txt");
+    //return B("");
 }
 
 BString BoxedContainer::getCacheFolder() {
